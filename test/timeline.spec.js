7c57093cc5d8f4e38917b88fefb04bc069abe49f
Mary Rose Cook <mary@maryrosecook.com>
2016-12-06 10:46:58 +0000

    Fix timeline tests for new transpose API

--- test/timeline.spec.js
+++ test/timeline.spec.js
@@ -5,13 +5,11 @@ describe("timeline", function() {
     it("doesn't transp earlier frame if drawing", function() {
       var now = 7;
       var frameTime = 5;
-      var playStart = 3;
       var firstFrameTime = 3;
       var lastFrameTime = 7;
       expect(timeline.transposeFrameTimeToCurrentLoop(
         now,
         frameTime,
-        playStart,
         firstFrameTime,
         lastFrameTime)).toEqual(frameTime);
     });
@@ -19,13 +17,11 @@ describe("timeline", function() {
     it("transps mid-anim frame when drawing over", function() {
       var now = 21;
       var frameTime = 4;
-      var playStart = 3;
       var firstFrameTime = 3;
       var lastFrameTime = 7;
       expect(timeline.transposeFrameTimeToCurrentLoop(
         now,
         frameTime,
-        playStart,
         firstFrameTime,
         lastFrameTime)).toEqual(19);
     });
@@ -33,13 +29,11 @@ describe("timeline", function() {
     it("transps mid-anim frame when drawing over", function() {
       var now = 22;
       var frameTime = 7;
-      var playStart = 3;
       var firstFrameTime = 3;
       var lastFrameTime = 7;
       expect(timeline.transposeFrameTimeToCurrentLoop(
         now,
         frameTime,
-        playStart,
         firstFrameTime,
         lastFrameTime)).toEqual(22);
     });
